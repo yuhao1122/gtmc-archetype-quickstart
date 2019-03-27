0088a0b76c155e9a2b12a34aca2a31e9c741062c
yuhao1122 <1194752204@qq.com>
2019-03-27 12:20:43 +0800

    test

--- src/main/java/com/gtmc/archetypes/controller/TestController.java
+++ src/main/java/com/gtmc/archetypes/controller/TestController.java
@@ -13,6 +13,8 @@ public class TestController {
 
     @GetMapping("/")
     public String test() {
+        System.out.println(1111);
+        System.err.println(2222);
         return "Hello World12345";
     }
 
